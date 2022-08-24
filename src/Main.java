b652bc3937b060758db2c982e04d3686510eee88
Hoang Chu <kamikiburning2002@gmail.com>
2022-08-24 13:55:28 -0500

    Intro

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java1refresher.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book1 = new Book();
+        Book book2 = new Book("Python", "Marc Hauschildt", false, 800);
+        Book book3 = new Book("Java A Beginner's Guide", "Herbert", true, 688);
+//        System.out.println(book1);
+//        System.out.println(book2);
+//        System.out.println(book3);
+//        System.out.println(book3.getAuthor());
+        Book[] bookself = new Book[4];
+        bookself[0] = book1;
+        bookself[1] = book2;
+        bookself[2] = book3;
+
+        for (Book book: bookself) {
+            System.out.println(book);
+        }
+
+        for (int i = 0; i < Book.getBookCount(); i++) {
+            System.out.println(bookself[i]);
+        }
     }
 }
---
+++ src/assignment/intro/AssignmentIntroMain.java
@@ -0,0 +1,7 @@
+package assignment.intro;
+
+public class AssignmentIntroMain {
+    public static void main(String[] args) {
+
+    }
+}
