27e03f3d3efb6b71312cb95a87bc9f09f83c61d9
Hoang Chu <kamikiburning2002@gmail.com>
2022-08-24 21:54:55 -0500

    Finish Intro Assignment

--- src/assignment/intro/AssignmentIntroMain.java
+++ src/assignment/intro/AssignmentIntroMain.java
@@ -2,6 +2,24 @@ package assignment.intro;
 
 public class AssignmentIntroMain {
     public static void main(String[] args) {
+        try {
+            Version version;
+            //version = new Version();
+            //version = new Version("1.2.3");
+            version = new Version("1.9.3.4");
+            //version = new Version("1.-5.3.d");
+            //version = new Version("1");
+            //version = new Version("1.1");
+            //version = new Version(2, -7, 6);
+            //version = new Version("a.b.c");
+            //version = new Version("");
 
+            System.out.println(version.getMajor());
+            System.out.println(version.getMinor());
+            System.out.println(version.getPatch());
+            System.out.println(version);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/assignment/intro/Version.java
+++ src/assignment/intro/Version.java
@@ -1,4 +1,86 @@
 package assignment.intro;
 
 public class Version {
+    private int major;
+    private int minor;
+    private int patch;
+
+    public Version() {
+        major = 0;
+        minor = 0;
+        patch = 1;
+    }
+
+    public Version(String version) {
+        String[] ver = version.split("\\.");
+        if (ver.length == 1 && ver[0] == "") {
+            patch = 1;
+        } else if (ver.length == 1) {
+            setMajor(toNumber(ver[0]));
+        } else if (ver.length == 2) {
+            setMajor(toNumber(ver[0]));
+            setMinor(toNumber(ver[1]));
+        } else {
+            setMajor(toNumber(ver[0]));
+            setMinor(toNumber(ver[1]));
+            setPatch(toNumber(ver[2]));
+        }
+    }
+
+    public Version(int major, int minor, int patch) {
+        setMajor(major);
+        setMinor(minor);
+        setPatch(patch);
+    }
+
+    public int getMajor() {
+        return major;
+    }
+
+    public void setMajor(int major) {
+        if (major < 0) {
+            throw new IllegalArgumentException("Major can not be a negative number");
+        } else {
+            this.major = major;
+        }
+    }
+
+    public int getMinor() {
+        return minor;
+    }
+
+    public void setMinor(int minor) {
+        if (minor < 0) {
+            throw new IllegalArgumentException("Minor can not be a negative number");
+        } else {
+            this.minor = minor;
+        }
+    }
+
+    public int getPatch() {
+        return patch;
+    }
+
+    public void setPatch(int patch) {
+        if (patch < 0) {
+            throw new IllegalArgumentException("Patch can not be a negative number");
+        } else {
+            this.patch = patch;
+        }
+    }
+
+    private int toNumber(String s) {
+        int num;
+        try {
+            num = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Can not convert string to a number!");
+        }
+        return num;
+    }
+
+    @Override
+    public String toString() {
+        return "Version: " + major + "." + minor + "." + patch;
+    }
 }
